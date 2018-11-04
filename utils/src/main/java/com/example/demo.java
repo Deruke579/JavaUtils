80510800232d5d601adec40176b7a6d8a583c8c3
jinqiu-huang <1711697715@qq.com>
2018-11-04 08:06:16 +0800

    init utils module

---
+++ utils/src/main/java/com/example/demo.java
@@ -0,0 +1,10 @@
+package com.example;
+
+/**
+ * Created by Kelly on 2018/11/4.
+ */
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub");
+    }
+}
