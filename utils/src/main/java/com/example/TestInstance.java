db14c752e780053d5a0911e5d77f25d0a2f3a909
KellyJin <1711697715@qq.com>
2019-04-02 17:53:39 +0800

    测试一些公式

---
+++ utils/src/main/java/com/example/TestInstance.java
@@ -0,0 +1,27 @@
+package com.example;
+
+/**
+ * Created by Kelly on 2019/3/28.
+ */
+public class TestInstance {
+    public static void main(String[] args) {
+//        int i=0;
+//        int n=++i;
+//        System.out.println(i);//1
+//        System.out.println(n);//1
+//        System.out.println(++i);//2
+//        int j=0;
+//        int m=j++;
+//        System.out.println(j);//1
+//        System.out.println(m);//0
+//        System.out.println(j++);//1
+//        byte i=Byte.MAX_VALUE;//-127
+//        byte n= Byte.MIN_VALUE;//-128
+//        System.out.println(i);
+//        byte j=++i;
+//        System.out.println(j);
+
+
+
+    }
+}
